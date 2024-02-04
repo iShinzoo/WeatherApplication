72ae9dbdca24c61754adffcc17f90dbbef8afe6c
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-02-04 23:08:05 +0530

    logic and some ui

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.weatherapplication.dataModel.RetrofaceBuilder
 
 
@@ -16,17 +20,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var weatherViewModelFactory: WeatherViewModelFactory
     private lateinit var loader : ProgressBar
 
+    private lateinit var edtCityName : EditText
+    private lateinit var weatherRegion : TextView
+    private lateinit var weatherIcon : ImageView
+    private lateinit var waetherCondition : TextView
+    private lateinit var btnSearch : ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         init()
 
-        val cityName = "Delhi"
-        weatherViewModel.getWeatherDetail(cityName)
+
+        btnSearch.setOnClickListener( View.OnClickListener {
+            weatherViewModel.getWeatherDetail(edtCityName.text.toString())
+        })
 
         weatherViewModel.weatherDetailLiveData.observe(this){
-            Log.i("WeatherDetail",it.toString())
+
+            val currentWeatherType = it.current.condition.text
+            val currentTempInC = it.current.temp_c
+            val imageLink = "https:${it.current.condition.icon}"
+            val currentCity = it.location.name
+            val currentState = it.location.region
+
+            weatherRegion.text = "$currentCity , $currentState"
+            waetherCondition.text = "$currentWeatherType , $currentTempInC"
+
+            Glide.with(this)
+                .load(imageLink)
+                .into(weatherIcon)
         }
 
         weatherViewModel.isLoading.observe(this){
@@ -43,5 +68,11 @@ class MainActivity : AppCompatActivity() {
         weatherViewModelFactory = WeatherViewModelFactory(repo)
         weatherViewModel = ViewModelProvider(this,weatherViewModelFactory).get(WeatherViewModel::class.java)
         loader = findViewById(R.id.loader)
+        edtCityName = findViewById(R.id.searchbar)
+        weatherRegion = findViewById(R.id.weatherRegion)
+        weatherIcon = findViewById(R.id.weatherIcon)
+        waetherCondition = findViewById(R.id.weatherCondition)
+        btnSearch = findViewById(R.id.btnSearch)
     }
 }
+
